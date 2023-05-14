e196a90c9ef9bb7840135d13572a1d4fae9e6ece
BuildTools <unconfigured@null.spigotmc.org>
2023-05-14 15:02:12 +0200

    first commit

---
+++ git-pull.lua
@@ -0,0 +1,15 @@
+-- Config
+local username = "LordAntonius"
+local reponame = "myrepo"
+local branch   = "master"
+
+-- Code
+local url = "https://raw.githubusercontent.com/" .. username .. "/" .. reponame .. "/" .. branch .."/"
+if table.maxn(arg) == 0 then
+    print("Usage : git-pull <files>")
+else
+    for k,v in pairs(arg) do
+        shell.run("rm", v)
+        shell.run("wget",url .. v)
+    end
+end
